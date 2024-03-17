c6d62c0d529274e6c5757a2967e315bd95a931ef
Pouya Ghajardavalou <ghajar.pouya@gmail.com>
2024-03-17 08:03:33 +0100

    Add files via upload

---
+++ ft_rev_int_tab.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		temp = tab[i];
+		tab[i] = tab[size - i - 1];
+		tab[size - i - 1] = temp;
+		i++;
+	}
+}
